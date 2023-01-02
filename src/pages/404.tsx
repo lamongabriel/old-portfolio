687421c15f2c7e5cdfb62f2f04b905fa9d719b31
lamongabriel <gabriel-lamon@outlook.com>
2023-01-02 13:43:40 -0300

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Box } from '../components/design/Box'
+import { HeadingGradient } from '../components/design/HeadingGradient'
+import { Layout } from '../components/design/Layout'
+
+export default function NotFound () {
+  const router = useRouter()
+
+  const buttonClasses = `
+		rounded
+		mt-4
+		cursor-pointer
+		text-center
+		text-lg-white-500
+		border-2
+		border-lg-primary-green
+		py-2
+		transition-colors
+		hover:bg-lg-primary-green
+	`
+
+  return (
+
+		<Layout className='flex justify-center items-center'>
+			<Box className='max-w-md w-full flex flex-col'>
+				<HeadingGradient className='text-2xl mx-auto w-fit'>
+					404.
+				</HeadingGradient>
+				<div onClick={() => router.back()} className={buttonClasses}>
+					Voltar
+				</div>
+				<Link href="/" className={buttonClasses}>
+					Home
+				</Link>
+			</Box>
+		</Layout>
+  )
+}
