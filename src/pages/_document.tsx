c541479fcca6243e9bf18f141fc8326adc22f3ae
lamongabriel <gabriel-lamon@outlook.com>
2023-01-06 20:44:59 -0300

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document () {
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
 
 				<meta name="theme-color" content="#151515" />
+				<link rel="shortcut icon" href="/images/favicon.ico" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+				<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       </Head>
       <body>
         <Main />
