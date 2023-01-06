b81367b6785575cd2e4e69be6514aa2385b90570
lamongabriel <gabriel-lamon@outlook.com>
2023-01-06 16:04:09 -0300

    colors for theme and body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document () {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossorigin" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
+				<meta name="theme-color" content="#151515" />
       </Head>
       <body>
         <Main />
