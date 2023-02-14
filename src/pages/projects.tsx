a3470ad1932dd227e37402768648a9922de11670
Gabriel Lamon <gabriel-lamon@outlook.com>
2023-02-14 10:49:52 -0300

    more projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import { HeadingGradient } from '../components/design/HeadingGradient'
 import { Layout } from '../components/design/Layout'
 import { Text } from '../components/design/Text'
@@ -6,7 +8,9 @@ import { ProjectCard } from '../components/ProjectCard'
 import straightNotes from '../assets/projects/straight-notes.png'
 import spacetraveling from '../assets/projects/spacetraveling-blog.png'
 import ignews from '../assets/projects/ignews.png'
-import Head from 'next/head'
+import inovaki from '../assets/projects/inovaki-home.png'
+import dtmoney from '../assets/projects/dt-money.png'
+import termo from '../assets/projects/termo.png'
 
 export default function Projects () {
   return (
@@ -33,7 +37,7 @@ export default function Projects () {
 				<Text>
 					Alguns dos projetos que desenvolvi. Amo a prática, é a melhor forma de desenvolver novas habilidades e aprender novas ferramentas.
 				</Text>
-				<section className='my-10 flex flex-col lg:flex-row gap-6 justify-center items-stretch'>
+				<section className='my-10 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
 					<ProjectCard
 						title='Straight Notes'
 						image={straightNotes}
@@ -60,6 +64,32 @@ export default function Projects () {
 					>
 						Blog com sistema de matérias por assinatura, onde o usuário deve pagar para consumir o conteúdo.
 						Feito utilizando NextJS, SSG, FaunaDB e a API de pagamentos Stripe.
+					</ProjectCard>
+					<ProjectCard
+						title='Inovaki'
+						image={inovaki}
+						link='https://inovaki.vercel.app/'
+					>
+						Ao lado de meus parceiros, desenvolvi com NextJS o novo site para a agência de programação Inovaki - Serviços de Informação na Internet.
+						Priorizando o SEO esse site foi construído para promover os diversos serviços da agência.
+					</ProjectCard>
+					<ProjectCard
+						title='Wordle Clone'
+						image={termo}
+						link='https://lamongabriel.github.io/termo-wordle-clone/'
+						github='https://github.com/lamongabriel/termo-wordle-clone'
+					>
+						Já jogou Wordle? o famoso jogo de palavras do NY Times? este foi traduzido e se chama termo,
+						desenvolvi um clone do site Termo com lógica totalmente nova utilizando somente JavaScript.
+					</ProjectCard>
+					<ProjectCard
+						title='DT Money'
+						image={dtmoney}
+						link='https://lamongabriel-dtmoney.vercel.app/'
+						github='https://github.com/lamongabriel/dt-money'
+					>
+						App para gerenciar melhor suas finanças, feito com a API Fake MirageJS e ReactJS, esse aplicativo irá te ajudar
+						a controlar melhor o seu dinheiro recebido e o seu dinheiro gasto.
 					</ProjectCard>
 				</section>
 			</Layout>
